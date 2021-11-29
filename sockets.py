d7dcd64d8cf1999a80402800a25f551774b2dd05
Zach Hong <44427485+ZacOrz@users.noreply.github.com>
2021-11-28 17:36:29 -0700

    re update
    re update to let it it run in windows

--- sockets.py
+++ sockets.py
@@ -60,6 +60,7 @@ class World:
         return self.space
 
 myWorld = World()        
+clients = list()  
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
@@ -69,19 +70,43 @@ myWorld.add_set_listener( set_listener )
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return flask.redirect('/static/index.html')
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            msg = ws.receive()
+            print(f"WS RECV: {msg}")
+            if (msg is not None):
+                packet = json.loads(msg)
+                msg = json.dumps(packet)
+                for client in clients:
+                    client.put(msg)
+    except Exception as e:
+        print(e)
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn(read_ws, ws, client)
+    print("Subscribing")
+    try:
+        while True:
+            msg = client.get()
+            print("Got a message")
+            ws.send(msg)
+    except Exception as e:
+        print(f"WS Error occured: {e}")
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
+    #return None
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,23 +124,29 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    data = flask_post_json()
+    myWorld.set(entity, data)
+    world_entity = myWorld.get(entity)
+    return json.dumps(world_entity)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return json.dumps(myWorld.__dict__['space'])
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    world_entity = myWorld.get(entity)
+    return json.dumps(world_entity)
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    myWorld.clear_listeners()
+    return json.dumps(myWorld.__dict__['space'])
 
 
 
